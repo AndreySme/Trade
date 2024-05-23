4847ee60ee6c87fd5f2cf5403c82600291b417c2
Andrey Smelkov <104786028+AndreySme@users.noreply.github.com>
2024-05-23 19:37:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ avr = st.text_input(label='Введите следующую цену')
 low = st.text_input(label='Введите нижнюю цену')
 
 btn = st.button(label='Выполнить', type="primary")
-result = high.float() + avr.float()
+result = high + avr
 if btn:
     st.write(result)
 
