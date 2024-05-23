e1ffe1e4f586107d40cb70a47d17a0801af2d615
Andrey Smelkov <104786028+AndreySme@users.noreply.github.com>
2024-05-23 15:29:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ import streamlit as st
 high = st.text_input(label='Введите верхнюю цену')
 avr = st.text_input(label='Введите следующую цену')
 low = st.text_input(label='Введите нижнюю цену')
-if st.button(label='Выполнить', type="primary")
+if st.button(label='Выполнить', type="primary"):
     st.write(high)
 
